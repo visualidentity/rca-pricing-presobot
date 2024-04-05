2cb450145a0639f4129a3a97950a124c0a008833
Dan <daniel.phan@vi.com.au>
2024-04-05 16:04:18 +1100

    REA26589: Build slides

--- src/js/hooks/selections.js
+++ src/js/hooks/selections.js
@@ -4,7 +4,19 @@ export default {
   onSave({ sections, data, context, feeds }) {
     const slides = _.flatMap(sections, 'slides');
 
+    // Hide these slides if data empty
+    sections.forEach((section) => {
     
+      if(section.key == 'fy25_pricing') {
+        section.slides.forEach((slide) => {
+          // Show/hide particular slide with its endpoint
+          const isAgencyROISummaryHidden = slide.key === 'fy25_roi_summary' && !feeds.riosummaryNewListings?.totalNewListings;
+          if (
+            isAgencyROISummary
+          ) slide.visible = false;
+        });
+      }
+    });
 
     return sections;
   }
